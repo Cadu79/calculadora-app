0db5863360e73ee637d098fa4dcb3f747f26ed52
Cadu 79 <eduardoprimogenito@gmail.com>
2024-06-02 15:44:14 -0300

    Criação dos botões da Calculadora.

--- calculadora-app/src/components/Calculadora.vue
+++ calculadora-app/src/components/Calculadora.vue
@@ -4,25 +4,45 @@
 
 <template>
   <div class="calculadora">
-    <div>Teste</div>
-    <div>Teste</div>
-    <div>Teste</div>
-    <div>Teste</div>
-    <div>Teste</div>
-    <div>Teste</div>
-    <div>Teste</div>
-    <div>Teste</div>
-    <div>Teste</div>
-    <div>Teste</div>
-    <div>Teste</div>
-    <div>Teste</div>
+    <div class="display">0</div>
+    <div>C</div>
+    <div>+ / -</div>
+    <div>%</div>
+    <div>÷</div>
+    <div>7</div>
+    <div>8</div>
+    <div>9</div>
+    <div>X</div>
+    <div>4</div>
+    <div>5</div>
+    <div>6</div>
+    <div>-</div>
+    <div>1</div>
+    <div>2</div>
+    <div>3</div>
+    <div>+</div>
+    <div>0</div>
+    <div>=</div>
   </div>
 </template>
 
 <style scoped>
 .calculadora {
+  font-size: 40px;
   display: grid;
+  width: 350px;
+  text-align: center;
   grid-template-columns: repeat(4, 1fr);
   grid-auto-rows: minmax(50px, auto);
+}
+
+.display {
+  grid-column: 1 / 5;
+  background-color: #333;
+  color: white;
+  display: flex;
+  justify-content: flex-end;
+  align-items: center;
+  padding: 0 10px;
 }
 </style>
