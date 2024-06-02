207d91ae80eb69c91450bfa813e7fb1f187ba849
Cadu 79 <eduardoprimogenito@gmail.com>
2024-06-02 15:20:27 -0300

    Início do desenvolvimento da Calculadora.

--- calculadora-app/src/App.vue
+++ calculadora-app/src/App.vue
@@ -1,40 +1,20 @@
 <script setup>
-import Calculadora from './components/Calculadora.vue'
-
+import Calculadora from './components/Calculadora.vue';
 </script> 
 
 <template>
   <div id="app">
-    <img src="./assets/logo.svg" alt="">
     <Calculadora />
   </div>
 </template>
 
 <style scoped>
-header {
-  line-height: 1.5;
-}
-
-.logo {
-  display: block;
-  margin: 0 auto 2rem;
-}
-
-@media (min-width: 1024px) {
-  header {
-    display: flex;
-    place-items: center;
-    padding-right: calc(var(--section-gap) / 2);
-  }
-
-  .logo {
-    margin: 0 2rem 0 0;
-  }
-
-  header .wrapper {
-    display: flex;
-    place-items: flex-start;
-    flex-wrap: wrap;
-  }
+#app {
+  font-family: 'Avenir', Arial, Helvetica, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
 }
 </style>
--- calculadora-app/src/components/Calculadora.vue
+++ calculadora-app/src/components/Calculadora.vue
@@ -1,44 +1,28 @@
 <script setup>
-defineProps({
-  msg: {
-    type: String,
-    required: true
-  }
-})
+// Nenhum código JavaScript no script setup, o que está correto se não houver lógica a ser adicionada.
 </script>
 
 <template>
-  <div class="greetings">
-    <h1 class="green">{{ msg }}</h1>
-    <h3>
-      You’ve successfully created a project with
-      <a href="https://vitejs.dev/" target="_blank" rel="noopener">Vite</a> +
-      <a href="https://vuejs.org/" target="_blank" rel="noopener">Vue 3</a>.
-    </h3>
+  <div class="calculadora">
+    <div>Teste</div>
+    <div>Teste</div>
+    <div>Teste</div>
+    <div>Teste</div>
+    <div>Teste</div>
+    <div>Teste</div>
+    <div>Teste</div>
+    <div>Teste</div>
+    <div>Teste</div>
+    <div>Teste</div>
+    <div>Teste</div>
+    <div>Teste</div>
   </div>
 </template>
 
 <style scoped>
-h1 {
-  font-weight: 500;
-  font-size: 2.6rem;
-  position: relative;
-  top: -10px;
-}
-
-h3 {
-  font-size: 1.2rem;
-}
-
-.greetings h1,
-.greetings h3 {
-  text-align: center;
-}
-
-@media (min-width: 1024px) {
-  .greetings h1,
-  .greetings h3 {
-    text-align: left;
-  }
+.calculadora {
+  display: grid;
+  grid-template-columns: repeat(4, 1fr);
+  grid-auto-rows: minmax(50px, auto);
 }
 </style>
